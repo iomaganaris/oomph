93952cbfe92d41902afa147800e6d7d10a3c442c
Ioannis Magkanaris <iomagkanaris@gmail.com>
2026-07-13 13:21:54 +0200

    Fixes for hwmalloc and clang compilation

--- cmake/oomph_mpi.cmake
+++ cmake/oomph_mpi.cmake
@@ -9,6 +9,9 @@ if (OOMPH_WITH_MPI)
     add_library(oomph_mpi SHARED)
     add_library(oomph::mpi ALIAS oomph_mpi)
     oomph_shared_lib_options(oomph_mpi)
+    if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        target_link_libraries(oomph_mpi PUBLIC atomic)
+    endif()
     install(TARGETS oomph_mpi
         EXPORT oomph-targets
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
